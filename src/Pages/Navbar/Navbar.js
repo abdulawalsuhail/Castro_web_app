a92547946628db2a4c693ebfb3744439fde4a5a3
abdul awal suhail <abdullahsuhail43@gmail.com>
2023-01-18 02:39:06 +0600

    navbar responsive done

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <div class="navbar pt-7">
+            {/* <div class="navbar pt-7 bg-slate-400">
                 <div class="flex-1 pl-40">
                     <a class="btn btn-ghost normal-case text-3xl">daisyUI</a>
                 </div>
@@ -17,38 +17,21 @@ const Navbar = () => {
                         <p>your address goes here</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
             {/* upper nav access */}
 
-            <div class="navbar bg-base-100">
+            <div class="navbar bg-orange-100">
                 <div class="navbar-start">
                     <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabindex="0">
-                        <a class="justify-between">
-                            Parent
-                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                        </a>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                    </div>
-                    
-                </div>
-                <div class="navbar-center hidden lg:flex">
-                    <ul class="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
                     <li tabindex="0">
                         <a>
-                        Parent
+                        Service
                         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                         </a>
                         <ul class="p-2">
@@ -56,7 +39,67 @@ const Navbar = () => {
                         <li><a>Submenu 2</a></li>
                         </ul>
                     </li>
-                    <li><a>Item 3</a></li>
+                    <li tabindex="0">
+                        <a>
+                        Project
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul class="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li tabindex="0">
+                        <a>
+                        Blog
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul class="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Contact</a></li>
+                    </ul>
+                    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    </div>
+                    
+                </div>
+                <div class="navbar-center hidden  lg:flex">
+                    <ul class="menu menu-horizontal pr-96">
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li tabindex="0">
+                        <a>
+                        Service
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul class="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li tabindex="0">
+                        <a>
+                        Project
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul class="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li tabindex="0">
+                        <a>
+                        Blog
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul class="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Contact</a></li>
                     </ul>
                 </div>
                 
